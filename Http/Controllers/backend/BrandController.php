85d8d38c8fc3c3465c2e59cbdd9f0f2325ae5fee
Md. Saiful Alam <82754863+saifulalam2559@users.noreply.github.com>
2024-06-08 03:27:11 +0200

    Update BrandController.php

--- Http/Controllers/backend/BrandController.php
+++ Http/Controllers/backend/BrandController.php
@@ -7,187 +7,147 @@ use Illuminate\Http\Request;
 use App\Models\Brand;
 use Illuminate\Support\Str;
 
-
-
 class BrandController extends Controller
 {
- 
+    /*****************************************
+   E-commerce shop products brand
+  ****************************************/
     public function index()
     {
-        $brands = Brand::orderBy('id','DESC')->get();
-        return view('backend.brand.index', compact('brands'));
+        $brands = Brand::orderBy("id", "DESC")->get();
+        return view("backend.brand.index", compact("brands"));
     }
 
-    public function brandStatus(Request $request) {
-        
-        
-        if($request->mode == 'true'){
-            
-            Brand::where('id',$request->id)->update(['status'=>'active']);
-           
-        }else{
-            
-            Brand::where('id',$request->id)->update(['status'=>'inactive']);
+    public function brandStatus(Request $request)
+    {
+        if ($request->mode == "true") {
+            Brand::where("id", $request->id)->update(["status" => "active"]);
+        } else {
+            Brand::where("id", $request->id)->update(["status" => "inactive"]);
         }
-        
-        return response()->json(['msg'=>'Status erfolgreich aktualisiert','status'=>true]);
-        
+
+        return response()->json([
+            "msg" => "Status erfolgreich aktualisiert",
+            "status" => true,
+        ]);
     }
-  
-    
+
     public function create()
     {
-        return view('backend.brand.create');
+        return view("backend.brand.create");
     }
 
-
-    
     public function store(Request $request)
     {
-        
-        
-        $this->validate($request, [
-            
-            'title'=> 'string|required ',
-            'photo'=> 'required ',
-            'status'=> 'nullable|in:active,inactive',
-        
-        ], 
-        
-         [
-            'title.string' => 'Bitte verwenden Sie eine Zeichenfolge im Titel',
-            'title.required' => 'Sie müssen Ihren Namen eingeben',
-            'photo.required' => 'Ohne Foto ist dies nicht möglich',
-        ] );
-               
-        
-         $data = $request->all();
-         $slug = Str::slug($request->input('title'));
-         $slug_count = Brand::where('slug',$slug)->count();
-         
-         if($slug_count>0){
-             
-             $slug = time().'-'.$slug;
-             
-         }
-         
-       
-         $data['slug'] = $slug ;
-         $status = Brand::create($data);
-         
-         if($status){
-             
-             return redirect()->route('brand.index')->with('success','Marke wurde erfolgreich erstellt!');
-             
-         }else{
-             
-             return redirect()->back()->with('error','Etwas ist schiefgelaufen');
-         }
-         
+        $this->validate(
+            $request,
+            [
+                "title" => "string|required ",
+                "photo" => "required ",
+                "status" => "nullable|in:active,inactive",
+            ],
+
+            [
+                "title.string" =>
+                    "Bitte verwenden Sie eine Zeichenfolge im Titel",
+                "title.required" => "Sie müssen Ihren Namen eingeben",
+                "photo.required" => "Ohne Foto ist dies nicht möglich",
+            ]
+        );
+
+        $data = $request->all();
+        $slug = Str::slug($request->input("title"));
+        $slug_count = Brand::where("slug", $slug)->count();
+
+        if ($slug_count > 0) {
+            $slug = time() . "-" . $slug;
+        }
+
+        $data["slug"] = $slug;
+        $status = Brand::create($data);
+
+        if ($status) {
+            return redirect()
+                ->route("brand.index")
+                ->with("success", "Marke wurde erfolgreich erstellt!");
+        } else {
+            return redirect()
+                ->back()
+                ->with("error", "Etwas ist schiefgelaufen");
+        }
     }
 
-
-    
     public function show($id)
     {
         //
     }
 
-
-    
     public function edit($id)
     {
-        
-        
         $band = Brand::find($id);
-        
-        if($band){
-            
-            return view('backend.brand.edit', compact('band'));
-            
-        }else{
-            
-            return back()->with('error','Data not found!!');
-            
+
+        if ($band) {
+            return view("backend.brand.edit", compact("band"));
+        } else {
+            return back()->with("error", "Data not found!!");
         }
-        
     }
 
-
-    
     public function update(Request $request, $id)
     {
-        
-        
-            $band = Brand::find($id);
-        
-        if($band){
-            
-             $this->validate($request, [
-            
-            'title'=> 'string|required ',
-            'photo'=> 'required ',
-            'status'=> 'nullable|in:active,inactive',
-        
-        ], 
-        
-         [
-            'title.string' => 'Bitte verwenden Sie eine Zeichenfolge im Titel',
-            'title.required' => 'Sie müssen Ihren Namen eingeben',
-            'photo.required' => 'Ohne Foto ist dies nicht möglich',
-        ] );
-               
-        
-         $data = $request->all();
-         
-         $status = $band->fill($data)->save();
-         
-         if($status){
-             
-             return redirect()->route('brand.index')->with('success','Marke wurde aktualisiert!');
-             
-         }else{
-             
-             return redirect()->back()->with('error','Etwas ist schiefgelaufen');
-         }
-            
-        }else{
-            
-            return back()->with('error','Daten nicht gefunden!!');
-            
+        $band = Brand::find($id);
+
+        if ($band) {
+            $this->validate(
+                $request,
+                [
+                    "title" => "string|required ",
+                    "photo" => "required ",
+                    "status" => "nullable|in:active,inactive",
+                ],
+
+                [
+                    "title.string" =>
+                        "Bitte verwenden Sie eine Zeichenfolge im Titel",
+                    "title.required" => "Sie müssen Ihren Namen eingeben",
+                    "photo.required" => "Ohne Foto ist dies nicht möglich",
+                ]
+            );
+
+            $data = $request->all();
+
+            $status = $band->fill($data)->save();
+
+            if ($status) {
+                return redirect()
+                    ->route("brand.index")
+                    ->with("success", "Marke wurde aktualisiert!");
+            } else {
+                return redirect()
+                    ->back()
+                    ->with("error", "Etwas ist schiefgelaufen");
+            }
+        } else {
+            return back()->with("error", "Daten nicht gefunden!!");
         }
-        
-        
-        
     }
 
-
-    
     public function destroy($id)
     {
-        
-        
-                $band = Brand::find($id);
-        
-        if($band){
-            
+        $band = Brand::find($id);
+
+        if ($band) {
             $status = $band->delete();
-            
-            if($status){
-                
-                return redirect()->route('brand.index')->with('success','Marke wurde gelöscht!!');
-                
-            }else{
-                
-                return back()->with('error','Etwas ist schiefgelaufen!!');
-                
+
+            if ($status) {
+                return redirect()
+                    ->route("brand.index")
+                    ->with("success", "Marke wurde gelöscht!!");
+            } else {
+                return back()->with("error", "Etwas ist schiefgelaufen!!");
             }
-            
-        }else{
-            
-            return back()->with('error','Daten nicht gefunden!');
-            
+        } else {
+            return back()->with("error", "Daten nicht gefunden!");
         }
-        
     }
 }
